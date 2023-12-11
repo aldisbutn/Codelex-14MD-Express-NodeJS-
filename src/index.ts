fd29e332b3b8c4281ffbce95b992b9c8204a8a00
Aldis Poga <aldis.poga@gmail.com>
2023-12-11 10:47:24 +0200

    small changes

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,12 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/drivers', async (req, res) => {
-  // Execute the query to get all drivers
   connection.query('SELECT * FROM drivers', (error, results) => {
     if (error) {
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
 
-    // Send the drivers as a JSON response
     res.json(results);
   });
 });
@@ -37,8 +35,7 @@ app.post('/drivers', async (req, res) => {
     return;
   }
 
-  connection.query(
-    `
+  connection.query(`
   INSERT INTO drivers (driverName, racesWon, favTrack, teamName, photoURL, createdAt)
   VALUES ('${driverName}', '${racesWon}', '${favTrack}', '${teamName}', '${photoURL}', '${createdAt}');
   `,
@@ -82,8 +79,7 @@ app.put('/drivers/:id', async (req, res) => {
 app.delete('/drivers/:id', async (req, res) => {
   const id = parseInt(req.params.id);
 
-  connection.query(
-    `
+  connection.query(`
   DELETE FROM drivers WHERE id = ?
   `,
     id,
